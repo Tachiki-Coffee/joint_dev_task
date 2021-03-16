9409dece7cad40a20818a8547334c7d3f0d0e3ee
kohei <tk1124e25@gmail.com>
2021-03-17 01:09:28 +0900

    q15,q16の解答記入

--- task.rb
+++ task.rb
@@ -131,7 +131,17 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  if data1.key?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
   
+  if data2.key?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
 end
 
 def q16
@@ -143,7 +153,10 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  # 2次元ハッシュ！！！！
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
