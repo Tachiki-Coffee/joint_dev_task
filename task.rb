7cc37f4c398a3d2e3d6e87079d4550efdd760964
kohei <tk1124e25@gmail.com>
2021-03-17 21:05:50 +0900

    q11,q15の解答の修正

--- task.rb
+++ task.rb
@@ -97,8 +97,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports = sports.flatten.uniq
-  #p sports
+  sports.flatten!.uniq!
   sports.each.with_index(1) do |sport, num|
     puts "No#{num} #{sport}"
   end
@@ -131,17 +130,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
-  
-  if data2.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data1.key?(:age)? "OK" : "NG"
+  puts data2.key?(:age)? "OK" : "NG"
 end
 
 def q16
@@ -153,7 +143,6 @@ def q16
   ]
 
   # 以下に回答を記載
-  # 2次元ハッシュ！！！！
   users.each do |user|
     puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
   end
