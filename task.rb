a761adad6d3ce17c030c7111b25f91f846ae9768
kohei <tk1124e25@gmail.com>
2021-05-04 10:55:46 +0900

    Ruby課題応用Q20の解答

--- task.rb
+++ task.rb
@@ -202,7 +202,7 @@ end
 
 class Item
   # 以下を修正して下さい
-  attr_accessor :name
+  attr_reader :name
   def initialize(name:)
     @name = name
   end
@@ -216,12 +216,34 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  attr_reader :name, :age
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 end
-
+#case user.age
+#
+##{user.name}の入場料金は#{entry_fee}円です
 class Zoo
   # 以下に回答を記載
-
+  attr_reader :entry_fee
+  def initialize(name:, entry_fee:)
+    @name = name
+    @entry_fee = entry_fee
+  end
+  def info_entry_fee(user)
+    case user.age 
+    when 0..5
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:infant]} 円です。"
+    when 6..12
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:children]} 円です。"
+    when 13..64
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:adult]} 円です。"
+    when 65..120
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:senior]} 円です。"
+    end
+  end
 end
 
 def q20
